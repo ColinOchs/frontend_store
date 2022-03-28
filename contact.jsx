e7d63204f51809726ef120f1891f71a9995694eb
ColinOchs <97154394+ColinOchs@users.noreply.github.com>
2022-03-27 20:48:32 -0800

    Add files via upload

---
+++ contact.jsx
@@ -0,0 +1,27 @@
+
+
+import "./contact.css";
+
+const Contact = () => {
+
+    return(
+        
+            <div className="contactInfo">
+                <h1>Feel free to reach out to us!</h1>
+            <ul>
+              <li> Email: </li> 
+              <li>  Phone:</li> 
+              <li> Address:</li> 
+
+              <li> Facebook:</li> 
+              <li>LinkedIn:</li> 
+              <li>Instagram:</li> 
+            </ul>
+            </div>
+
+     
+    )
+
+};
+
+export default Contact;
---
+++ footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer  = () => {
+    return (<div className="footer">
+        <h2>&copy; 2022, ColinTech Industries</h2><h4><p>Developer: Colin Ochs</p></h4>
+        
+    </div>);
+};
+
+export default Footer;
+
